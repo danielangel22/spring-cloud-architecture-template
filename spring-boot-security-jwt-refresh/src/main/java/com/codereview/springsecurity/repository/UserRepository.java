be5488c56d5d79cbe4c80f228f60b10f4cf3f1a0
Daniel Angel <daniel.angel@elaniin.com>
2022-04-26 11:42:31 -0600

    :zap: add spring security with jwt and eureka

---
+++ spring-boot-security-jwt-refresh/src/main/java/com/codereview/springsecurity/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.codereview.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codereview.springsecurity.model.DAOUser;
+
+@Repository
+public interface UserRepository extends JpaRepository<DAOUser, Long> {
+	DAOUser findByUsername(String username);
+
+}
